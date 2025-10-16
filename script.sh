5100ee81e5ba529b236e8d6177c73a5362135790
Siddharth Rajesh <163349416+SiddharthRajesh2003@users.noreply.github.com>
2025-10-16 19:42:40 -0400

    Added commands to process the dataset

--- script.sh
+++ script.sh
@@ -9,3 +9,12 @@ python -m graph_builder.curvature_pipeline --dataset_file data/dataset_PathNet.p
 
 python -m graph_builder.curvature_pipeline --dataset_file data/dataset_PPNet.pkl --method both \
         --augment --num_views 2 --elimination_ratio 0.2 --strategy priority
+
+python -m graph_builder.curvature_pipeline --dataset_file data/dataset_GGNet.pkl  --method both \
+        --augment --num_views 2 --elimination_ratio 0.2 --strategy random
+
+python -m graph_builder.curvature_pipeline --dataset_file data/dataset_PathNet.pkl --method both \
+        --augment --num_views 2 --elimination_ratio 0.2 --strategy random
+
+python -m graph_builder.curvature_pipeline --dataset_file data/dataset_PPNet.pkl --method both \
+        --augment --num_views 2 --elimination_ratio 0.2 --strategy random
