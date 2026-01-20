df589a2586610e283b0e63fc96d4a79c1120377b
Siddharth Rajesh <163349416+SiddharthRajesh2003@users.noreply.github.com>
2026-01-20 13:15:43 -0500

    .

--- preprocess_script.sh
+++ preprocess_script.sh
@@ -2,6 +2,33 @@ module load conda
 conda activate gnn_env
 
 python -m graph_builder.curvature_pipeline --dataset_file data/dataset_GGNet.pkl  --method both \
+        --augment --num_views 2 --elimination_ratio 0.1 --strategy priority --use_kfold
+
+python -m graph_builder.curvature_pipeline --dataset_file data/dataset_PathNet.pkl --method both \
+        --augment --num_views 2 --elimination_ratio 0.1 --strategy priority --use_kfold
+
+python -m graph_builder.curvature_pipeline --dataset_file data/dataset_PPNet.pkl --method both \
+        --augment --num_views 2 --elimination_ratio 0.1 --strategy priority --use_kfold
+
+python -m graph_builder.curvature_pipeline --dataset_file data/dataset_GGNet.pkl  --method both \
+        --augment --num_views 2 --elimination_ratio 0.1 --strategy random --use_kfold
+
+python -m graph_builder.curvature_pipeline --dataset_file data/dataset_PathNet.pkl --method both \
+        --augment --num_views 2 --elimination_ratio 0.1 --strategy random --use_kfold
+
+python -m graph_builder.curvature_pipeline --dataset_file data/dataset_PPNet.pkl --method both \
+        --augment --num_views 2 --elimination_ratio 0.1 --strategy random --use_kfold
+
+python -m graph_builder.curvature_pipeline --dataset_file data/dataset_GGNet.pkl  --method both \
+        --augment --num_views 2 --elimination_ratio 0.1 --strategy coarsening --use_kfold
+
+python -m graph_builder.curvature_pipeline --dataset_file data/dataset_PathNet.pkl --method both \
+        --augment --num_views 2 --elimination_ratio 0.1 --strategy coarsening --use_kfold
+
+python -m graph_builder.curvature_pipeline --dataset_file data/dataset_PPNet.pkl --method both \
+        --augment --num_views 2 --elimination_ratio 0.1 --strategy coarsening --use_kfold
+
+python -m graph_builder.curvature_pipeline --dataset_file data/dataset_GGNet.pkl  --method both \
         --augment --num_views 2 --elimination_ratio 0.2 --strategy priority --use_kfold
 
 python -m graph_builder.curvature_pipeline --dataset_file data/dataset_PathNet.pkl --method both \
